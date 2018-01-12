9c7a2ec5af09a738582bd63741d6c2131e9e883b
alireza ashrafi <alirezaashrafi.email@gmail.com>
2018-01-12 13:08:23 +0330

    change files

--- library/src/main/java/com/alirezaashrafi/library/interfaces/OnComplete.java
+++ library/src/main/java/com/alirezaashrafi/library/interfaces/OnComplete.java
@@ -1,8 +1,12 @@
 package com.alirezaashrafi.library.interfaces;
 
+import android.graphics.Bitmap;
+
 /**
  * pico Created by AlirezaAshrafi on 1/11/2018.
  */
 
 public interface OnComplete {
+    void onComplete(Bitmap result);
+
 }
--- library/src/main/java/com/alirezaashrafi/library/interfaces/OnProgress.java
+++ library/src/main/java/com/alirezaashrafi/library/interfaces/OnProgress.java
@@ -4,5 +4,7 @@ package com.alirezaashrafi.library.interfaces;
  * pico Created by AlirezaAshrafi on 1/11/2018.
  */
 
-public interface OnProgress {
+public interface OnProgress
+{
+    void progress(int progress);
 }
